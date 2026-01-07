2969e0b48a7cf8eeeed8a8ec680b859de9710d93
RisyaDeviyanti <risyadeviyanti0201@gmail.com>
2026-01-07 10:57:09 +0700

    Add files via upload

---
+++ NO 1 risya.py
@@ -0,0 +1,12 @@
+nama = input("Risya Deviyanti: ")
+usia = input("18: ")
+alamat = input("Girimaya: ")
+hobi = input("Bernyanyi: ")
+
+print(f"Nama    : (Risya Deviyanti)")
+print(f"Usia    : (18) tahun")
+print(f"Alamat  : (Girimaya)")
+print(f"Hobi    : (Bernyanyi)")
+
+# Menjalankan fungsi utama
+# tampilkan_biodata()
